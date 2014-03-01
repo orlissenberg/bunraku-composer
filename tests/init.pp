672f530014401cf766040cd50da8bb575d837dac
O. R. Lissenberg <olissenberg@gmail.com>
2014-03-01 18:16:51 +0100

    Working test manifest.

--- tests/init.pp
+++ tests/init.pp
@@ -8,5 +8,8 @@
 #
 # Learn more about module testing here: http://docs.puppetlabs.com/guides/tests_smoke.html
 #
+# sudo puppet apply /vagrant/submodules/bunraku-composer/tests/init.pp
+
+import '../manifests/init.pp'
 
 class { "composer": }
